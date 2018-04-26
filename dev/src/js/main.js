06bd5d58ffd509213051e9dc6405aa604fc667c8
char1eschen <martian2005@gmail.com>
2018-04-27 08:56:07 +1000

    add slideshow

---
+++ dev/src/js/main.js
@@ -0,0 +1,14 @@
+/*
+* @Author: char1eschen
+* @Date:   2018-04-25 10:18:39
+* @Last Modified by:   char1eschen
+* @Last Modified time: 2018-04-27 08:54:20
+*/
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+  });
+});
